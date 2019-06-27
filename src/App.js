ac78c5a367586afb54089c9962ac1f02ebeb5227
Vincent Man <vincentman2702@hotmail.com>
2019-06-27 12:36:35 +0100

    fix fetch request

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+// import './Utilities/KeyFrames.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      readings: { 
-        gasPrev: 11000, gas: 0, gasUsed: 0, gasBill: 0,
-        elecPrev: 11000, elec: 0, elecUsed: 0, elecBill: 0 }
+      gas: {
+        prev: 11000, current: 0, used: 0, bill: 0,
+      },
+      elec: {
+        prev: 11000, current: 0, used: 0, bill: 0,
+      }
     };
   }
 
   componentDidMount() {
-    fetch(`https://shine-energy.netlify.com/.netlify/functions/meter-readings`, { mode: 'no-cors'} )
+    fetch(`https://cors-anywhere.herokuapp.com/shine-energy.netlify.com/.netlify/functions/meter-readings`, {
+      headers: {
+        "x-requested-with": "XMLHttpRequest",
+        "origin": "https://google.com"
+
+      }
+    },
+    )
       .then(res => {
         return res.json();
       })
       .then(res => {
-        console.log('This is our response  ' + res);
+        console.log('This is our response  ' + Object.entries(res));
         this.setState(() => ({
-          readings: { 
-            gasPrev: res.gas.reading,
-            elecPrev: res.electricity.reading
-          }
+          gas: { prev: res.gas.reading, current: 0, used: 0, bill: 0 },
+          elec: { prev: res.electricity.reading, current: 0, used: 0, bill: 0 },
+          
         }))
       }).catch(err => {
         console.log("Our fetch request is failing, error: " + err);
@@ -33,11 +44,11 @@ class App extends React.Component {
     const reading = +event.target.value;
     const name = event.target.name;
     this.setState(() => ({
-      readings: {
-        [name + 'Prev']: this.state.readings[name + 'Prev'],
-        [name]: reading,
-        [name + 'Used']: reading - this.state.readings[name + 'Prev'],
-        [name +'Bill']: (100 * 10) + ((reading - this.state.readings[name + 'Prev'] - 100) * 20)
+      [name]: {
+        prev: this.state[name].prev,
+        current: reading,
+        used: reading - this.state[name].prev,
+        bill: (100 * 10) + ((reading - this.state[name].prev - 100) * 20)
       }
     }));
   }
@@ -45,23 +56,58 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <label htmlFor='gas'>Gas Reading:</label>
-        <input name='gas' type='number'
-          onChange={e => this.handleInput(e)} />
-        
-        <div className='container'>
-          <div className='item'>Gas Reading Entered: {this.state.readings.gas}</div>
-          <div className='item'>Gas units used => Current: {this.state.readings.gas} Previous: {this.state.readings.gasPrev} => Used: {this.state.readings.gasUsed}</div>
-          <div className='item'>Gas bill amount => {this.state.readings.gasBill}p (£{this.state.readings.gasBill / 100})</div>
+        <h1 className='title'>SHINE ENERGY</h1>
+        <h2 className='subtitle'>Your Account Summary</h2>
+
+        <div className='container container1'>
+          <div className='item'>
+            <label htmlFor='gas'>Gas Reading:</label>
+            <input name='gas' type='number'
+              onChange={e => this.handleInput(e)} /></div>
+          <div className='item'>Gas Reading Entered:
+            <span>{this.state.gas.current}</span></div>
+          <div className='item'>Gas units used =>
+
+            <div className='used'>
+              Current:
+            <span>{this.state.gas.current}</span>
+              <br />
+              previous:
+            <span>{this.state.gas.prev}</span> <br />Used:
+            <span>{this.state.gas.used}</span>
+            </div>
+
+          </div>
+          <div className='item'>Gas bill amount =>
+            <span>{this.state.gas.bill}</span>p (£
+            <span id='pound'>{this.state.gas.bill / 100}</span>)
+          </div>
         </div>
 
-        <label htmlFor='elec'>Electricity Reading:</label>
-        <input name='elec' type='number' 
-          onChange={e => this.handleInput(e)}/>
         <div className='container'>
-          <div className='item'>Electricty Reading Entered: {this.state.readings.elec}</div>
-          <div className='item'>Electricty units used => Current: {this.state.readings.elec} Previous: {this.state.readings.elecPrev} => Used: {this.state.readings.elecUsed}</div>
-          <div className='item'>Electricty bill amount => {this.state.readings.elecBill}p (£{this.state.readings.elecBill / 100})</div>
+          <div className='item'>
+            <label htmlFor='elec'>Electricity Reading:</label>
+            <input name='elec' type='number'
+              onChange={e => this.handleInput(e)} />
+          </div>
+          <div className='item'>Electricty Reading Entered: <span>{this.state.elec.current}</span></div>
+          <div className='item'>Electricty units used =>
+
+          <div className='used'>
+              Current:
+            <span>{this.state.elec.current}</span>
+              <br />
+              previous:
+              <span>{this.state.elec.prev}</span> => <br />Used:
+              <span>{this.state.elec.used}</span>
+            </div>
+          </div>
+
+          <div className='item'>Electricty bill amount =>
+            <span>{this.state.elec.bill}</span>p (£
+            <span id='pound'>{this.state.elec.bill / 100}</span>)
+          </div>
+
         </div>
       </div>
     );

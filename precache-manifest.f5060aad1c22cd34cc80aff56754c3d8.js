e0f89af6b96ea1272096a4400de32dcd82cce515
Vincent Man <vincentman2702@hotmail.com>
2019-06-27 14:43:10 +0100

    Updates

---
+++ precache-manifest.f5060aad1c22cd34cc80aff56754c3d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab230843c5f1648a2d26218aca992b7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fc1f349424e816a9b453",
+    "url": "/react-gh-pages/static/css/main.43fda347.chunk.css"
+  },
+  {
+    "revision": "d13dc0e792738a0e824d",
+    "url": "/react-gh-pages/static/js/2.2d43439a.chunk.js"
+  },
+  {
+    "revision": "fc1f349424e816a9b453",
+    "url": "/react-gh-pages/static/js/main.e3572fd4.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);

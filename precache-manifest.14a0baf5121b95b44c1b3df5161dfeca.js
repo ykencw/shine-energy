8c0704ad58cd3a8bde6864d2c021acf10adb5808
Richard Lan <ykencw@gmail.com>
2019-06-27 15:04:41 +0100

    Updates

---
+++ precache-manifest.14a0baf5121b95b44c1b3df5161dfeca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cba110fc6ab19800270f4ec85c4c901",
+    "url": "/shine-energy/index.html"
+  },
+  {
+    "revision": "ee51a75dddb17c586492",
+    "url": "/shine-energy/static/css/main.70785b87.chunk.css"
+  },
+  {
+    "revision": "9ab2fa6de350a941bcd0",
+    "url": "/shine-energy/static/js/2.65c4bffa.chunk.js"
+  },
+  {
+    "revision": "ee51a75dddb17c586492",
+    "url": "/shine-energy/static/js/main.5957a8dd.chunk.js"
+  },
+  {
+    "revision": "9f54a618d2269401d1c3",
+    "url": "/shine-energy/static/js/runtime~main.2555065e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.f5060aad1c22cd34cc80aff56754c3d8.js"
+  "/shine-energy/precache-manifest.14a0baf5121b95b44c1b3df5161dfeca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shine-energy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
